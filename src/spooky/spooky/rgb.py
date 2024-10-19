ba92648c1ad6a118409a49a285b291b0ec0d515a
Porter Clevidence <116387727+porterclev@users.noreply.github.com>
2024-10-19 02:23:29 -0700

    Porter/spooky prep (#8)
    * vim temp files
    
    * change colors on move

--- src/spooky/spooky/rgb.py
+++ src/spooky/spooky/rgb.py
@@ -2,17 +2,30 @@ import can
 import rclpy
 import time
 from rclpy.node import Node
-# from std_msgs.msg import String
+from sensor_msgs.msg import Joy
+from std_msgs.msg import String, UInt8
 
 
 class rgb(Node):
 
     def __init__(self):
         super().__init__('rgb_leds')
-        timer_period = 4  # seconds
+        timer_period = 1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.bus = can.interface.Bus(interface='socketcan', channel='can0', bitrate='500000')
         self.values = [0] * 8
+        self.subscription = self.create_subscription(
+			Joy,
+			'joy',
+			self.listener_callback,
+			10)
+
+
+        self.drive = [0, 0]
+    
+    def listener_callback(self, msg):
+        self.drive[0] = msg.axes[1]
+        self.drive[1] = msg.axes[3]
 
     def can_publish(self, arbitration_id, data, is_extended_id) -> None:
         can_msg = can.Message(
@@ -26,7 +39,7 @@ class rgb(Node):
     # also returns two variables for (AB)
     def conval(self, value):
         # just so everyone knows, this conversion was confusing as hell
-        prog = int(1000 - (value * (1000 - 500) / 255))
+        prog = 1000 - int(value * (1000 / 255))
         x = (prog >> 8) & 0xFF
         y = prog & 0xFF
         return x, y
@@ -42,14 +55,13 @@ class rgb(Node):
     # can method to publish the can message, derrived from the topmost method
     # theoretically should reproduce the [0,0,0,0,0,0,0,0]
     def timer_callback(self):
-        self.setRGB(255, 0, 0)
-        time.sleep(1)
-        self.setRGB(0, 255, 0)
-        time.sleep(1)
-        self.setRGB(0, 0, 255)
-        time.sleep(1)
-
+        if(self.drive[0] != 0 or self.drive[0] != 0):
+            self.setRGB(255, 0, 80)
+        else:
+            self.setRGB(255, 10, 0) # orange
+ 
 def main(args=None):
+    print("rgb start")
     rclpy.init(args=args)
 
     rgb_leds = rgb()
