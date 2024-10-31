26eeaffb1372fc2ce13e214aeee741a5ffc3ed05
Porter Clevidence <116387727+porterclev@users.noreply.github.com>
2024-10-30 19:11:33 -0700

    Vesc Controls via RPM (#9)

--- src/test_package/test_package/main_drive.py
+++ src/test_package/test_package/main_drive.py
@@ -4,6 +4,7 @@ from sensor_msgs.msg import Joy
 from std_msgs.msg import UInt8, String
 #ignore can import error if it's there, it works if you installed python-can
 import can
+from .vesc import Vesc as vesc
 
 
 class DrivetrainExcavator(Node):
@@ -40,42 +41,6 @@ class DrivetrainExcavator(Node):
         s = String()
         s.data = "Alive"
         self.activity_publisher_.publish(s)
-    # Converts Controller Speed to byte array (decimal form)
-    # Alg: signal -> percentage * 1000 (UInt16) -> Hexadecimal Byte Form -> Decimal Byte Form 
-    # Ex. 200 -> 50% -> 50,000 = [80, 200]
-    def signal_conversion(self, msg_data: int, bytes_range: int, frequency_floor: int) -> list[int]:
-        data: int = msg_data 
-        temp_data: list[int] = []
-
-        #make sure the direction is correct
-        if data > 100:
-            #increment for 2's comp
-            c = 1
-            # Forward msg correction:
-            data -= 100
-            # covert controller signal to proper range (1000-100000)
-            data *= frequency_floor
-
-            #convert to byte array but also 2's compliment to reverse motor
-            for i in range(bytes_range - 1, -1, -1):
-                temp_data.append(255 - ((data >> (8*i)) & 0xff))
-
-            for i in range(len(temp_data) - 1, - 1, -1):
-                temp_data[i] += c 
-                if temp_data[i] > 255:
-                    temp_data[i] = 0
-                else:
-                    c = 0
-                    break
-        else:
-            # covert controller signal to proper range (1000-100000)
-            data *= frequency_floor
-    
-            # convert signal to byte array
-            for i in range(bytes_range - 1, -1, -1):
-                temp_data.append((data >> (8*i)) & 0xff)
-        
-        return temp_data 
 
     def can_publish(self, arbitration_id, data, is_extended_id) -> None:
         can_msg = can.Message(
@@ -99,8 +64,6 @@ class DrivetrainExcavator(Node):
         if self.ex_dt_right_speed == msg.data:
             return None
         self.ex_dt_right_speed = msg.data
-
-
     
     # Conveyer Belt
     def ex_conveyer_update(self, msg):
@@ -108,7 +71,7 @@ class DrivetrainExcavator(Node):
             return None
         self.ex_conveyer_speed = msg.data
 
-        temp_data = self.signal_conversion(msg.data, 8, 10)
+        temp_data = vesc.signal_conversion(msg.data, 8, 10)
 
         self.can_publish(19, temp_data, True) 
 
@@ -117,7 +80,7 @@ class DrivetrainExcavator(Node):
         if self.ex_arm_speed == msg.data:
             return None
         self.ex_arm_speed = msg.data 
-        temp_data = self.signal_conversion(msg.data, 8, 10)
+        temp_data = vesc.signal_conversion(msg.data, 8, 10)
 
         self.can_publish(21, temp_data, True)
     
@@ -125,7 +88,7 @@ class DrivetrainExcavator(Node):
         if self.ex_digger_speed == msg.data:
             return None
         self.ex_digger_speed = msg.data 
-        temp_data = self.signal_conversion(msg.data, 8, 10)
+        temp_data = vesc.signal_conversion(msg.data, 8, 10)
 
         self.can_publish(23, temp_data, True)
 
@@ -135,20 +98,20 @@ class DrivetrainExcavator(Node):
             return None
         self.ex_servo_speed = msg.data
 
-        temp_data = self.signal_conversion(msg.data, 8, 100)
+        temp_data = vesc.signal_conversion(msg.data, 8, 100)
 
         self.can_publish(25, temp_data, True) 
 
     def timer_callback(self):
-        temp_data = self.signal_conversion(self.ex_dt_left_speed, 4, 1000)  # Has to be 4 to work on vesc
+        temp_data = vesc.signal_conversion(self.ex_dt_left_speed, 4, 1000)  # Has to be 4 to work on vesc
         # can message for right and left motor
-        self.can_publish(15, temp_data, True)
-        self.can_publish(16, temp_data, True) 
+        self.can_publish(vesc.id_conversion(15, 3), temp_data, True)
+        self.can_publish(vesc.id_conversion(16, 3), temp_data, True) 
 
         # converts controller signal to bytes array
-        temp_data = self.signal_conversion(self.ex_dt_right_speed, 4, 1000)  # Has to be 4 to work on vesc
-        self.can_publish(17, temp_data, True)
-        self.can_publish(18, temp_data, True)
+        temp_data = vesc.signal_conversion(self.ex_dt_right_speed, 4, 1000)  # Has to be 4 to work on vesc
+        self.can_publish(vesc.id_conversion(17, 3), temp_data, True)
+        self.can_publish(vesc.id_conversion(18, 3), temp_data, True)
 
 def main(args=None):
     print("Bus Publisher Active New")
