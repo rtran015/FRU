beb9858de852c9e6dd8bf44b3b3cf7487580334f
porterclev <porterclev@gmail.com>
2025-01-01 16:59:50 -0800

    Imported LiDAR SDK and sample package

--- launch/main_launch.py
+++ launch/main_launch.py
@@ -4,6 +4,14 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
+                package="test_package",
+                executable="main_joy",
+            ),
+        Node(
+            package="test_package", 
+            executable="main_drive"
+            ),
+        Node(
             package='autonomous',
             executable='auto',
         ),
