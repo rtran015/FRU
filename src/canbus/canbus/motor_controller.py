a8d079c48ff564ee9a541ca75ab943bc308a3fdb
Luigi Blasquez <luigiblasquez@gmail.com>
2025-02-25 00:28:52 +0000

    import hopefully

--- src/canbus/canbus/motor_controller.py
+++ src/canbus/canbus/motor_controller.py
@@ -1,6 +1,54 @@
-def main():
-    print('Hi from canbus.')
+import can
+import rclpy
+import time
+from rclpy.node import Node
+from sensor_msgs.msg import Joy
+from std_msgs.msg import String, UInt8
+from .vesc import Vesc as vesc
 
+class motor_controller(Node):
+    # remember bit conversions are INT16 - Big Endian (AB)
+    def __init__ (self):
+        super().__init__('motor_controller')
+        # can bus yipie (switch channel name to 'vcan0' for virtual can testing)
+        self.bus = can.interface.Bus(interface='socketcan', channel='can0', bitrate='500000')
+        topic_list = {'dt_l_pub', 'dt_r_pub'}
+        for topic in topic_list:
+             self.create_subscription = self.create_subscription(
+                  Joy, 
+                  topic,
+                  self.listener_callback,
+                  10)
+             
+    def id_conversion(device_id: int, command_id: int)->int:
+        return (command_id << 8) | device_id
+
+    def can_publish(self, arbitration_id, data, is_extended_id) -> None:
+        can_msg = can.Message(
+                arbitration_id = arbitration_id,
+                data = data, 
+                is_extended_id = is_extended_id
+                ) 
+        self.bus.send(can_msg)    
+
+    def listener_callback(self, msg):
+        msg.data 
+
+        if msg.topic == 'dt_l_pub':
+            self.can_publish(self.id_conversion(15, 3), vesc.signal_conversion(msg.data, 4, 1), True)
+            self.can_publish(self.id_conversion(16, 3), msg.data, True)
+
+        elif msg.topic == 'dt_r_pub':
+            self.can_publish(self.id_conversion(17, 3), msg.data, True)
+            self.can_publish(self.id_conversion(18, 3), msg.data, True)
+
+def main(args=None):
+        rclpy.init(args=args)
+        node = motor_controller()
+        rclpy.spin(node)
+
+        node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
