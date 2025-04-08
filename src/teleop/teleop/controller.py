645c386f4ff86c8e1736c89fa5c10324d11abdff
porterclev <porterclev@gmail.com>
2025-04-07 17:44:43 -0700

    formatting

--- src/teleop/teleop/controller.py
+++ src/teleop/teleop/controller.py
@@ -1,93 +1,110 @@
 import rclpy
-from rclpy.node import Node 
-from sensor_msgs.msg import Joy 
+from rclpy.node import Node
+from sensor_msgs.msg import Joy
 from std_msgs.msg import UInt8
 from can import Message, Bus
+from rcl_interfaces.msg import ParameterDescriptor
+
 
 class JoyPub(Node):
     def __init__(self):
-        super().__init__('controller_publisher')
-        self.dt_l_pub = self.create_publisher(UInt8, 'dt_l_pub', 10)
-        self.dt_r_pub = self.create_publisher(UInt8, 'dt_r_pub', 10)
-        self.trap = self.create_publisher(UInt8, 'trap', 10)
-        self.ex_2_pub = self.create_publisher(UInt8, 'ex_2_pub', 10)
-        self.ex_1_pub = self.create_publisher(UInt8, 'ex_1_pub', 10)
-        self.dig_pub = self.create_publisher(UInt8, 'dig_pub', 10)
-        self.subscription = self.create_subscription(Joy, 'joy', self.listener_callback, 10)
+        super().__init__("controller_publisher")
+        self.dt_l_pub = self.create_publisher(UInt8, "dt_l_pub", 10)
+        self.dt_r_pub = self.create_publisher(UInt8, "dt_r_pub", 10)
+        self.trap = self.create_publisher(UInt8, "trap", 10)
+        self.ex_2_pub = self.create_publisher(UInt8, "ex_2_pub", 10)
+        self.ex_1_pub = self.create_publisher(UInt8, "ex_1_pub", 10)
+        self.dig_pub = self.create_publisher(UInt8, "dig_pub", 10)
+        self.subscription = self.create_subscription(
+            Joy, "joy", self.listener_callback, 10
+        )
         self.bucketSpeed = 0
         self.liftspeed = 0
 
         self.declare_parameters(
-            namespace='',
+            namespace="",
             parameters=[
-                ('deadzone', None),
-                ('speed_limit', None)
-            ]
+                (
+                    "deadband",
+                    None,
+                    ParameterDescriptor(
+                        description="Controls joystick senesitivity to start reading data"
+                    ),
+                ),
+                (
+                    "speed_limit",
+                    None,
+                    ParameterDescriptor(
+                        description="Controls Drive Train max speed (Percentage)"
+                    ),
+                ),
+            ],
         )
         self.descendSpeed = 0
-        # 100 = no speed
-    def listener_callback(self, msg:Joy):
+
+    def listener_callback(self, msg: Joy):
         uint8 = UInt8()
         if msg.axes[1] > self.deadband:
-            uint8.data = int(-msg.axes[1]*self.speed_limit + 100)
+            uint8.data = int(-msg.axes[1] * self.speed_limit + 100)
             self.dt_l_pub.publish(uint8)
         elif msg.axes[1] < -self.deadband:
-            uint8.data = int(-msg.axes[1]*self.speed_limit + 100)
+            uint8.data = int(-msg.axes[1] * self.speed_limit + 100)
             self.dt_l_pub.publish(uint8)
-        else: 
+        else:
             uint8.data = 100
             self.dt_l_pub.publish(uint8)
 
         if msg.axes[3] > self.deadband:
-            uint8.data = int(msg.axes[3]*self.speed_limit + 100)
+            uint8.data = int(msg.axes[3] * self.speed_limit + 100)
             self.dt_r_pub.publish(uint8)
         elif msg.axes[3] < -self.deadband:
-            uint8.data = int(msg.axes[3]*self.speed_limit+ 100)
+            uint8.data = int(msg.axes[3] * self.speed_limit + 100)
             self.dt_r_pub.publish(uint8)
         else:
             uint8.data = 100
             self.dt_r_pub.publish(uint8)
 
         if msg.buttons[0] == 1:
-            uint8.data = 100    
+            uint8.data = 100
             self.trap.publish(uint8)
         else:
             uint8.data = 0
-            self.trap.publish(uint0)
-            
-        if msg.buttons[1] == 1:  
+            self.trap.publish(uint8)
+
+        if msg.buttons[1] == 1:
             pass
 
-        if msg.buttons[2] == 1:  
+        if msg.buttons[2] == 1:
             pass
-          
-        if msg.buttons[3] == 1:  
+
+        if msg.buttons[3] == 1:
             pass
-            
-        #Button 5 (right bumper)
+
+        # i don't know if this is correct?
+        if msg.buttons[4] == 1:  # digging High (Left bumper)
+            if self.bucketSpeed != 0:
+                self.bucketSpeed -= 10
+                uint8.data = self.bucketSpeed
+            self.dig_pub.publish(uint8)
+
+        # Button 5 (right bumper)
         if msg.button[5] == 1:
             self.liftspeed += 10
             uint8.data = self.liftspeed
             self.ex_1_pub.publish(uint8)
-        
-        # Excavation Stage 2 Button (right trigger)
-        if msg.buttons[7] == 1:
-            self.descendSpeed = 30  #placeholder
-            uint8.data = self.descendSpeed
-            self.ex_2_pub.publish(uint8)
 
-        # i don't know if this is correct?
-        if msg.buttons[4] == 1: #digging High (Left bumper) 
-            if (self.bucketSpeed != 0):
-                self.bucketSpeed -= 10
-                uint8.data = self.bucketSpeed
-            self.dig_pub.publish(uint8)
-    
-        if msg.buttons[6] == 1: #digginh low  (Left trigger)
+        if msg.buttons[6] == 1:  # diggin low  (Left trigger)
             self.bucketSpeed += 10
             uint8.data = self.bucketSpeed
             self.dig_pub.publish(uint8)
-        
+
+        # Excavation Stage 2 Button (right trigger)
+        if msg.buttons[7] == 1:
+            self.descendSpeed = 30  # placeholder
+            uint8.data = self.descendSpeed
+            self.ex_2_pub.publish(uint8)
+
+
 def main():
     print("Controller On")
     rclpy.init(args=None)
@@ -97,5 +114,7 @@ def main():
 
     node.destroy_node()
     rclpy.shutdown()
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
